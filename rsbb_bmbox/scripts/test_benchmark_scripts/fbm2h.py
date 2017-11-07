bcc363cb29cc4c71d17d73c85af7cf5993d1fe6a
Enri2077 <enry2077@gmail.com>
2017-11-07 22:13:24 +0100

    deleted old files

--- rsbb_bmbox/scripts/test_benchmark_scripts/fbm2h.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from rockin_scoring.BmBox3 import BmBox
-from rockin_scoring.BenchmarkObject import BenchmarkObject
-
-script_object = BenchmarkObject("fbm2h")
